0b1a3ed3712d6b99d2fc8631064e53cf71417168
Ismail <113051912+kitsuyome@users.noreply.github.com>
2024-02-07 22:58:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,3 +2,4 @@ import gdown
 
 gdown.download("https://drive.google.com/uc?id=189dxgb6hfD1NKuqeFGmK7llnOZP770rH", "test_data/checkpoint.pth")
 gdown.download("https://drive.google.com/uc?id=1x3pFRU43QYwK4VviWn1WakRXNaAEAATe", "test_data/config.json")
+gdown.download("https://drive.google.com/uc?id=1QrTcbtN37QPU_jgg0YKXhWxcZzwAGriJ", "test_data/state_dict.pth")
